96053357b63c0393aa9603d397e6776c2569262d
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-07-13 18:36:51 +0530

    start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,12 @@ echo "Configuring the bot"
 git clone https://github.com/Noob-Stranger/andencento.git
 cd andencento
 
-echo " Setting Up And Starting "
+echo "‌‌‎    _              _                           _          _____     _ 
+   / \   _ __   __| | ___ _ __   ___ ___ _ __ | |_ ___   |___ /  __| |
+  / _ \ | '_ \ / _ |/ _ \ '_ \ / / _ \ '_ \| / _ \    |_ \ / _ |
+ / ___ \| | | | (_| |  __/ | | | (_|  __/ | | | || (_) |  ___) | (_| |
+/_/   \_\_| |_|\__,_|\___|_| |_|\___\___|_| |_|\__\___/  |____/ \__,_|                     
+
+"
 
 python3 -m userbot
